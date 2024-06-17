44592f95b911b8a6577c853e7782ad89dffd405a
Adriel Barria <barria.adriel66@gmail.com>
2024-06-17 01:11:06 -0300

    Correcciones
    Muchas Correcciones

--- Portfolio/src/app/app.component.spec.ts
+++ Portfolio/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'AdrielBarria_Portfolio' title`, () => {
+  it(`should have as title 'Portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AdrielBarria_Portfolio');
+    expect(app.title).toEqual('Portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AdrielBarria_Portfolio');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Portfolio');
   });
 });
