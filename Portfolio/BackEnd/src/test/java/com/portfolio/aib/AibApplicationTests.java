280d7c42dff08eb9e9f228e0b27517e0c496a92c
Adriel Barria <barria.adriel66@gmail.com>
2024-07-03 14:48:34 -0300

    Creacion Carpeta BackEnd
    - Se creo la carpeta BackEnd

---
+++ Portfolio/BackEnd/src/test/java/com/portfolio/aib/AibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.aib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
