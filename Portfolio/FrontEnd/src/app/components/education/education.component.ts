e06e80148a185e67d5a913998dd95c3e908dc55e
Adriel Barria <barria.adriel66@gmail.com>
2024-10-07 14:20:12 -0300

    Edits

--- Portfolio/FrontEnd/src/app/components/education/education.component.ts
+++ Portfolio/FrontEnd/src/app/components/education/education.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Educacion } from '../../model/educacion';
+import { EducacionService } from '../../service/educacion.service';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
-export class EducationComponent {
+export class EducationComponent implements OnInit{
+  educacion: Educacion[] = [];
+
+  constructor(private educacionS: EducacionService) { }
+  ngOnInit(): void {
+    this.cargarEducacion();
+    
+  }
+
+  cargarEducacion(): void{
+    this.educacionS.lista().subscribe(
+      data =>{
+        this.educacion = data;
+      }
+    )
+  }
+
+  delete(id?: number){
+    if( id != undefined){
+      this.educacionS.delete(id).subscribe(
+        data => {
+          this.cargarEducacion();
+        }, err => {
+          alert("No se pudo eliminar");
+        }
+      )
+    }
+  }
+  
 
 }
